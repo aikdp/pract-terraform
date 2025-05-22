1cdf444c868e70c33f2fe99008823cc2e8a7b0db
aikdp <kdprasad028@gmail.com>
2025-05-22 12:56:51 +0530

    Move to Practice

---
+++ vault-tf/main.tf
@@ -0,0 +1,35 @@
+provider "aws"{
+    region = "us-east-1"
+}
+
+provider "vault" {
+  address = "http://44.211.91.90:8200"
+  skip_child_token = true
+
+  auth_login {
+    path = "auth/approle/login"
+
+   parameters = {
+      role_id   = "75aefc79-5543-4f0e-caa4-714cfa9421e3"
+      secret_id = "d1dca7aa-ddf8-7659-4374-dcb5740cbc03"
+  }
+}
+}
+
+#data Source to read the existing data
+data "vault_kv_secret_v2" "example" {
+  mount = "kv"
+  name  = "test-secret"
+}
+
+#Now craete a resource
+
+resource "aws_instance" "main"{
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    # vpc_secuirty_group_ids = ["PASTE-SG-ID"]
+
+    tags = {
+        Secret = data.vault_kv_secret_v2.example.data["username"]
+    }
+}
